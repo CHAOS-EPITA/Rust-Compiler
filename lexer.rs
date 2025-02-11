6b9ca68d80f5bd2b1ee935204338d3fc4669de68
Marcus <marcus.rabourdin@epita.fr>
2025-02-12 00:12:54 +0100

    lexer_update

--- lexer.rs
+++ lexer.rs
@@ -15,7 +15,7 @@ enum Token {
     StringLit(String), // Chaînes de caractères
     Keyword(String), // Mots-clés (fn, let, if, else...)
     Plus, Minus, Star, Slash, // Opérateurs mathématiques
-    Eq, Neq, Le, Ge, Lt, Gt, // Comparaisons ==, !=, <=, >=, <, >
+    Eq, EqEq, Neq, Not, Le, Ge, Lt, Gt, // Comparaisons ==, !=, <=, >=, <, >
     LParen, RParen, LBrace, RBrace, Comma, Semicolon, Colon, Arrow, // Délimiteurs
     Comment(String), // Commentaires
     Eof, // Fin du fichier
@@ -39,9 +39,97 @@ impl Lexer{
     }
 
 
-    fn next_char(&mut self) -> Option<char>{}
-    fn peek_char(&self) -> Option<char>{}
-    fn next_token(&mut self) -> Token{}
+    fn next_char(&mut self) -> Option<char>{
+        if self.pos < self.input.len(){
+            let c = self.input[self.pos];
+            self.pos += 1;
+            Some(c)
+        } else {
+            None
+        }
+
+    }
+
+
+    fn peek_char(&self) -> Option<char>{
+        self.input.get(self.pos).copied()
+    }
+
+    fn next_token(&mut self) -> Token{
+        while let Some(c) = self.next_char(){
+            return match c{
+                ' ' | '\t' | '\r' => continue,
+                '+' => Token::Plus,
+                '-' =>{
+                    if self.peek_char() == Some('>'){
+                        self.next_char();
+                        Token::Arrow
+                    }
+                    else{
+                        Token::Minus
+                    }
+                }
+
+                '*' => Token::Star,
+                '/' => {
+                    self.peek_char() == Some('/'){
+                        self.consume_comment()
+                    } else{
+                        Token::Slash;
+                    }
+                }
+
+                '(' => Token::Lparen,
+                ')' => Token::RParen,
+                '{' => Token::LBrace,
+                '}' => Token::RBrace,
+                ',' => Token::Comma,
+                ';' => Token::Semicolon,
+                '=' => {
+                    if self.peek_char() == Some('='){
+                        self.next_char();
+                        Token::EqEq
+                    }else {
+                        Token::Eq
+                    }
+
+                }
+                '!' => {
+                    if self.peek_char() == Some('='){
+                        self.next_char();
+                        Token::Neq
+                    }else {
+                        Token:Not
+                    }
+                }
+
+                '<' => {
+                    if self.peek_char() == Some('='){
+                        self.peek_char();
+                        Token::Le
+                    }else {
+                        Token::Lt
+                    }
+                }
+
+                '>' => {
+                    if self.peek_char() == Some('='){
+                        self.next_char();
+                        Token::Ge
+                    }else{
+                        Token::Gt
+                    }
+                }
+                '"' => self.consume_string(),
+                '0'..='9' => self.consume_number(c),
+                'a'..='z' |'A'..='Z'| '_' => self.consume_iden(c),
+                _ => Token::Eof,
+            };
+
+        }
+        Token::Eof
+    }
+
     fn consume_comment(&mut self) -> Token{}
     fn consume_iden(&mut self, first: char) -> Token{}
     fn main(){}
