5fb0910a039c2d61eb1bfc2f845bbbac76f95e0b
Louis-Alexandre Charnay <louis-alexandre.charnay@epita.fr>
2025-02-24 19:38:42 +0100

    update-ir-llvm

--- backend/src/IR/llvm_ir.rs
+++ backend/src/IR/llvm_ir.rs
@@ -1,3 +1,4 @@
+//Première version
 /*extern crate llvm_sys as sys;
 extern crate llvm;
 
@@ -114,7 +115,9 @@ fn main() {
     create_llvm_ir();
 }*/
 
-extern crate llvm_sys as sys;
+
+//Deuxième version
+/*extern crate llvm_sys as sys;
 extern crate llvm;
 
 use llvm::prelude::*;
@@ -232,4 +235,138 @@ fn main() {
     );
 
     create_llvm_ir(&ast);
+}*/
+
+//Troisième version
+extern crate inkwell;
+
+use inkwell::builder::Builder;
+use inkwell::context::Context;
+use inkwell::module::Module;
+use inkwell::types::BasicTypeEnum;
+use inkwell::values::{BasicValueEnum, FunctionValue, IntValue};
+use std::process::exit;
+
+enum ASTNode {
+    BinaryOp(Box<ASTNode>, String, Box<ASTNode>),
+    Number(i32),
+    Function(String, Vec<String>, Box<ASTNode>),
+}
+
+fn create_llvm_ir(ast: &ASTNode) -> Result<(), String> {
+    let context = Context::create();
+
+    let module = context.create_module("example_module");
+
+    if let Err(e) = generate_llvm_ir(ast, &context, &module, &mut context.create_builder()) {
+        return Err(e);
+    }
+
+    if module.verify().is_err() {
+        return Err("Module verification failed!".to_string());
+    }
+
+    module.print_to_stderr();
+
+    if let Err(e) = module.write_to_file("example_module.bc") {
+        return Err(format!("Failed to write bitcode file: {}", e));
+    }
+
+    Ok(())
+}
+
+fn generate_llvm_ir(
+    ast: &ASTNode,
+    context: &Context,
+    module: &Module,
+    builder: &mut Builder,
+) -> Result<(), String> {
+    match ast {
+        ASTNode::Number(_) => Ok(()),
+        ASTNode::BinaryOp(left, op, right) => {
+            let left_value = generate_llvm_ir_expr(left, context, module, builder)?;
+            let right_value = generate_llvm_ir_expr(right, context, module, builder)?;
+
+            let result = match op.as_str() {
+                "+" => builder.build_add(left_value, right_value, "tmpadd"),
+                "-" => builder.build_sub(left_value, right_value, "tmpsub"),
+                _ => return Err("Unsupported operation".to_string()),
+            };
+
+            Ok(())
+        }
+        ASTNode::Function(name, args, body) => {
+            let param_types = args.iter().map(|_| context.i32_type().into()).collect::<Vec<_>>();
+            let fn_type = context.i32_type().fn_type(&param_types, false);
+            let fn_value = module.add_function(name, fn_type, None);
+
+            let entry = context.append_basic_block(fn_value, "entry");
+            builder.position_at_end(entry);
+
+            for (i, arg) in args.iter().enumerate() {
+                let param = fn_value.get_nth_param(i as u32).unwrap();
+                let alloca = builder.build_alloca(context.i32_type(), arg);
+                builder.build_store(param, alloca);
+            }
+
+            generate_llvm_ir(body, context, module, builder)?;
+
+            if let Some(ret_value) = generate_llvm_ir_expr(body, context, module, builder).ok() {
+                if let Some(int_value) = ret_value.into_int_value() {
+                    builder.build_return(Some(&int_value));
+                } else {
+                    return Err("Function body did not return an integer value".to_string());
+                }
+            } else {
+                return Err("Function body did not return a value".to_string());
+            }
+
+            Ok(())
+        }
+    }
+}
+
+fn generate_llvm_ir_expr(
+    ast: &ASTNode,
+    context: &Context,
+    module: &Module,
+    builder: &mut Builder,
+) -> Result<IntValue, String> {
+    match ast {
+        ASTNode::Number(value) => {
+            let int_type = context.i32_type();
+            let constant = int_type.const_int(*value as u64, false);
+            Ok(constant)
+        }
+        ASTNode::BinaryOp(left, op, right) => {
+            let left_value = generate_llvm_ir_expr(left, context, module, builder)?;
+            let right_value = generate_llvm_ir_expr(right, context, module, builder)?;
+
+            let result = match op.as_str() {
+                "+" => builder.build_add(left_value, right_value, "tmpadd"),
+                "-" => builder.build_sub(left_value, right_value, "tmpsub"),
+                _ => return Err("Unsupported operation".to_string()),
+            };
+
+            Ok(result)
+        }
+        _ => Err("Unsupported AST node in expression context".to_string()),
+    }
+}
+
+fn main() {
+    let ast = ASTNode::Function(
+        "add".to_string(),
+        vec!["a".to_string(), "b".to_string()],
+        Box::new(ASTNode::BinaryOp(
+            Box::new(ASTNode::Number(1)),
+            "+".to_string(),
+            Box::new(ASTNode::Number(2)),
+        )),
+    );
+
+    if let Err(e) = create_llvm_ir(&ast) {
+        eprintln!("Error: {}", e);
+        exit(1);
+    }
 }
