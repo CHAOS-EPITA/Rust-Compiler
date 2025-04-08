5cc7e55ba1ef291a58bd251fa1b23821c02f1952
Marcus <marcus.rabourdin@epita.fr>
2025-04-08 23:16:40 +0200

    finalVersionSoutenance

--- mini_rust_compiler/test.rs
+++ mini_rust_compiler/test.rs
@@ -2,10 +2,11 @@ fn main() {
 
     let x:i32 = 10;
     let y:i32 = 20;
+    println!("x = {}, y = {}",x,y);
     println!("Les opérateurs de base");
     println!("{} + {} = {}",x,y,sum(x,y));
     println!("{} - {} = {}",x,y,diff(x,y));
-    println!("{} / {} = {}",x,y,divide(x,y));
+    println!("{} / {} = {} (division entière)",x,y,divide(x,y));
     println!("{} * {} = {}",x,y,mult(x,y));
 
 }
@@ -15,5 +16,6 @@ fn sum(x:i32,y:i32) ->i32{ return x+y;}
 fn diff(x:i32,y:i32) ->i32{ return x-y;}
 fn divide(x:i32,y:i32) ->i32{ return x/y;}
 fn mult(x:i32,y:i32) ->i32{ return x*y;}
+
 
 
